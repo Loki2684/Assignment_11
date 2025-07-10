a11c8b3a04c7e09c338d78e2d0fd590f882e0767
Loki2684 <lokeshajgaonkar05@gmail.com>
2025-07-10 16:05:48 +0530

    Update CalculatorTest.java

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,12 +1,13 @@
+package com.example;
+ 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+ 
 public class CalculatorTest {
-
+ 
     @Test
     public void testAddition() {
         Calculator calc = new Calculator();
         int result = calc.add(2, 3);
         assertEquals(5, result);
     }
-}
