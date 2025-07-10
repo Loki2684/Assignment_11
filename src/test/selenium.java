46629007b6e0d106df251578a9a4564c4eaece00
Loki2684 <lokeshajgaonkar05@gmail.com>
2025-07-10 07:18:20 +0530

    Update selenium.java

--- src/test/selenium.java
+++ src/test/selenium.java
@@ -1,22 +1,27 @@
 package com.example;
- 
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
- 
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class SeleniumTest {
     @Test
     public void testGoogleHomepageTitle() {
-        // Set the path to your ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
- 
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // <-- Important!
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
- 
+
         String title = driver.getTitle();
         Assert.assertEquals("Google", title);
- 
+
         driver.quit();
     }
 }
